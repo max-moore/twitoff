2da3a4b5366c50c6d22a4a565d6352a9c727c6ea
Nicholas dela Fuente <51771867+nwdelafu@users.noreply.github.com>
2020-09-15 15:44:27 -0600

    found an error in my insert_example_users() - never created table

--- twitoff/app.py
+++ twitoff/app.py
@@ -4,36 +4,37 @@
 from flask import Flask, render_template
 from .models import DB, User
 from .twitter import  insert_example_users
+from os import getenv
 
 #function to create our app
 def create_app():
-  #initializing our flask app
-  app = Flask(__name__)
+    #initializing our flask app
+    app = Flask(__name__)
 
-  #for storing information in our database
-  app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///db.sqlite3'
-  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #for storing information in our database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-  #initilizes database within our app
-  DB.init_app(app)
+    #initilizes database within our app
+    DB.init_app(app)
 
-  #listens for path '/' and executes function when heard
-  @app.route('/')
-  def root():
-    return render_template('base.html', title="Home", users=User.query.all())
+    #listens for path '/' and executes function when heard
+    @app.route('/')
+    def root():
+        return render_template('base.html', title="Home", users=User.query.all())
 
-  #associated with update button
-  @app.route('/update')
-  def update():
-    insert_example_users()
-    return render_template('base.html', title="Updated Users", users=User.query.all())
+    #associated with update button
+    @app.route('/update')
+    def update():
+        insert_example_users()
+        return render_template('base.html', title="users updated!", users=User.query.all())
 
-  #associated with reset button
-  @app.route('/reset')
-  def reset():
-    DB.drop_all()
-    DB.create_all()
-    return render_template('base.html', title="DATABASE RESET")
-  
-  #returns that app with everything we are trying to render
-  return app
+    #associated with reset button
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title="Reset Database")
+
+    #returns that app with everything we are trying to render
+    return app
--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -8,7 +8,7 @@ from .models import DB, Tweet, User
 TWITTER_USERS = ['calebhicks', 'elonmusk', 'rrherr', 'SteveMartinToGo', 'alyankovic', 'nasa', 'sadserver', 'jkhowland', 'austen', 'common_squirrel', 'KenJennings', 'conanobrien', 'big_ben_clock', 'IAM_SHAKESPEARE']
 
 # This gives us authorization through .env file
-TWITTER_AUTH = tweepy.OAuthHandler(getenv('TWITTER_API_KEY')), getenv('TWITTER_API_KEY_SECRET'))
+TWITTER_AUTH = tweepy.OAuthHandler(getenv('TWITTER_API_KEY'), getenv('TWITTER_API_KEY_SECRET'))
 TWITTER = tweepy.API(TWITTER_AUTH)
 BASILICA = basilica.Connection(getenv('BASILICA_KEY'))
 
@@ -18,14 +18,12 @@ def add_or_update_user(username):
     #grabbing twitter user
     twitter_user = TWITTER.get_user(username)
     # add or update user
-    db_user = (User.query.get(twitter_user.id)) or 
-              User(id=twitter_user.id, name=username)
+    db_user = (User.query.get(twitter_user.id)) or User(id=twitter_user.id, name=username)
     DB.session.add(db_user)
 
     #grabbing tweet
-    tweets = twitter_user.timeline(count=200, exclude_replies=True, 
-                                   include_rts=False, tweet_mode='Extended',
-                                   since_id=db_user.newest_tweet_id)
+    tweets = twitter_user.timeline(count=200, exclude_replies=True, include_rts=False, 
+        tweet_mode='extended', since_id=db_user.newest_tweet_id)
 
     # if we get a new tweet then change the newest_tweet_id associated with ther user
     if tweets:
@@ -34,20 +32,22 @@ def add_or_update_user(username):
     #loops for tweets
     for tweet in tweets:
       embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
-      db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300], embedding=embedding)
+      db_tweet = Tweet(id=tweet.id, text=tweet.full_text, embedding=embedding)
       db_user.tweets.append(db_tweet)
       DB.session.add(db_tweet)
 
   except Exception as e: 
     print('ERROR PROCESSING {}: {}'.format(username, e))
     raise e
-  
+
   else:
     DB.session.commit()
 
 
 #populations using add_or_update user
 def insert_example_users():
+  DB.drop_all()
+  DB.create_all()
   add_or_update_user('elonmusk')
   add_or_update_user('jackblack')
 
